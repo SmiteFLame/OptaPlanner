5ce862240d03419b83e70b598787c8d5bd8aa30f
SmiteFLame <lukigreen@naver.com>
2022-02-06 14:45:49 +0900

    init opta planner

---
+++ src/main/kotlin/opta/planner/test/OptaPlannerApplication.kt
@@ -0,0 +1,11 @@
+package opta.planner.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OptaPlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<OptaPlannerApplication>(*args)
+}
---
+++ src/test/kotlin/opta/planner/test/OptaPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package opta.planner.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OptaPlannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
